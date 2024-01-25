a042a449cc2f5a91e1fd6c5eeaf7febf64895f26
120241400 <89237995+120241400@users.noreply.github.com>
2024-01-25 23:11:20 +0800

    Update 拳皇.js

--- zhanjs/拳皇.js
+++ zhanjs/拳皇.js
@@ -22,7 +22,7 @@ var rule = {
 		"img": ".aligncenter&&data-original",
 		"desc": ";;;.views:eq(0)&&Text;",
 		"content": "h1&&Text",
-		"tabs": "js:TABS=['👑运输车尊享👑']",
+		"tabs": "js:TABS=['拳皇赛事']",
 		"lists": `js:
 			var html = JSON.parse(request(input).match(/var bevideo_vids_.*?=({[\\s\\S]*?});/)[1]);
 			let list = [];
