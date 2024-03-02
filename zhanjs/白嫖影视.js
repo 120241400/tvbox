f4eebb4412249d40f4d45b13ff49f6938b930190
120241400 <89237995+120241400@users.noreply.github.com>
2024-03-02 08:50:36 +0800

    Update and rename 996影视.js to 白嫖影视.js

--- zhanjs/白嫖影视.js
+++ zhanjs/白嫖影视.js
@@ -1,6 +1,6 @@
 var rule= {
-    title:'996影视',
-    host:'https://www.cs1369.com',
+    title:'白嫖影视',
+    host:'https://www.baipiaoys.com',
     // url:'/index.php/vod/show/id/fyclass/page/fypage.html',
     url:'/show/id/fyclass/page/fypage.html',
     searchable: 2,//是否启用全局搜索,
