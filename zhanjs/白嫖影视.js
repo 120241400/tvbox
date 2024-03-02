35fdfd183c2cf8b07536b180258906ec6c1aee64
120241400 <89237995+120241400@users.noreply.github.com>
2024-03-02 08:58:26 +0800

    Update 白嫖影视.js

--- zhanjs/白嫖影视.js
+++ zhanjs/白嫖影视.js
@@ -1,6 +1,6 @@
 var rule= {
     title:'白嫖影视',
-    host:'https://www.baipiaoys.com',
+    host:'https://www.baipiaoys.cc',
     // url:'/index.php/vod/show/id/fyclass/page/fypage.html',
     url:'/show/id/fyclass/page/fypage.html',
     searchable: 2,//是否启用全局搜索,
